d6e30914b3d29378f13ea2362880e0d15daf00af
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2023-01-25 15:13:29 -0300

    added api to show files from current dir

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -6,4 +7,20 @@ app = FastAPI()
 def index():
     return {"message": "Hello World"}
     
+@app.get('/files')
+def showFiles():
+    entries = os.listdir('.')
+    for entry in entries:
+        # print(entry)
+        #if entry.is_file():
+        #    print("is file: " + entry.name)
+        #else:
+        #    print("is folder: " +  entry.name)    
+        filtered = []
+        for word in entries:
+            if "." in word:
+                filtered.append(word)
 
+        #filtered = filter(lambda file: '.' in file, entries)
+        #print(list(filtered))
+    return {"files": filtered}    
